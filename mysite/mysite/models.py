a8d421e2ecdb13d2ef319eaf30d55dc076f5ae86
mdevlin96 <monikardev@yahoo.com>
2019-11-05 14:48:07 -0800

    models file

---
+++ mysite/mysite/models.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.forms import ModelForm
+from django.db import models
+
+class NewSessionForm(models.Model):
+    sessionNum = models.CharField(min_length=1)
+    roomNum = models.CharField(min_length=1)
+    
+    project_name= models.CharField()
+    group_name= models.CharField()
+    advisor_name= model.CharField()
+  
+
+class PerGroupForm(models.Model):
+    sessionNum = models.IntegerField()
+    roomNum = models.IntegerField()
+    
+    project_name= models.ChoiceField()
+    group_name= models.ChoiceField()
+    advisor_name= models.ChoiceField()
+    
+    technical_accuracy= models.IntegerField()
+    creativity= models.IntegerField()
+    supporting_work= models.IntegerField()
+    design_process= models.IntegerField()
+    project_complexity= models.IntegerField()
+    completion= models.IntegerField()
+    tests= models.IntegerField()
+    response= models.IntegerField()
+    organization= models.IntegerField()
+    time= models.IntegerField()
+    visual= model.IntegerField()
+    confidence= model.IntegerField()
+    
+    overview= models.ChoiceField()
+    comments= models.CharField()
+    
+    
+    
+class PerSessionForm(models.Model):
+    discipline= forms.ChoiceField()
+    
+    q1= models.IntegerField()
+    q2= models.IntegerField()
+    q3= models.IntegerField()
+    q4= models.IntegerField()
+    q5= models.IntegerField()
+    q6= models.IntegerField()
+    q7= models.IntegerField()
+    q8= models.IntegerField()
+    q9= models.IntegerField()
+    q10= models.IntegerField()
+    q11= models.IntegerField()
+    q12= models.IntegerField()
+    
+    comments= models.CharField()
