703def3c829aa8146ee571762e0692d638f7751a
mdevlin96 <monikardev@yahoo.com>
2019-11-17 20:57:49 -0800

    create session/group urls

--- mysite/manager/urls.py
+++ mysite/manager/urls.py
@@ -27,34 +27,35 @@ urlpatterns = [
 	path('adminoptions/sendscores/', views.sendscores, name='sendscores'),
 	# Path to Admin Forms
 	path('adminoptions/adminforms/', views.adminforms, name='adminforms'),
-    # Path from the session form to add the session
-    path('adminoptions/addsession/', views.add_session, name='addsession'),
-    # Path to edit a session
-    path('edit_session/<int:session_id>/', views.edit_session, name='editasession'),
+	
+	# Path from the session form to add the session
+	path('adminoptions/addsession/', views.add_session, name='addsession'),
+	# Path to edit a session
+	path('edit_session/<int:session_id>/', views.edit_session, name='editasession'),
 
-    # Path to delete a session
-    path('delete_session/<int:session_id>/', views.delete_session, name='deletesession'),
+	# Path to delete a session
+	path('delete_session/<int:session_id>/', views.delete_session, name='deletesession'),
 
-    # Path to update a session
-    path('update_session/<int:session_id>/', views.update_session, name='updatesession'),
+	# Path to update a session
+	path('update_session/<int:session_id>/', views.update_session, name='updatesession'),
 
-    # Path to display session details
-    path('show_session/<int:session_id>/', views.show_session, name='showsession'),
-
-	# Path to add a new group
-   	path('add_group_form/', views.addgroupform, name='addgroupform'),
+	# Path to display session details
+	path('show_session/<int:session_id>/', views.show_session, name='showsession'),
 
 	# Path to add a new group
-   	path('add_group/', views.add_group, name='addgroup'),
+   	path('add_group_form/<int:session_id>/', views.addgroupform, name='addgroupform'),
 
-    # Path to edit a group
-    path('edit_group/<int:group_id>/', views.edit_group, name='editgroup'),
+	# Path to add a new group
+   	path('add_group/<int:session_id>/', views.add_group, name='addgroup'),
 
-    # Path to delete a group
-    path('delete_group/<int:group_id>/', views.delete_group, name='deletegroup'),
+	# Path to edit a group
+	path('edit_group/<int:group_id>/', views.edit_group, name='editgroup'),
 
-    # Path to update a group
-    path('update_group/<int:group_id>/', views.update_group, name='updategroup'),
+	# Path to delete a group
+	path('delete_group/<int:group_id>/', views.delete_group, name='deletegroup'),
+
+	# Path to update a group
+	path('update_group/<int:group_id>/', views.update_group, name='updategroup'),
 
 	# Path to edit a session
 	path('adminoptions/editsession/', views.editsession, name='editsession'),
